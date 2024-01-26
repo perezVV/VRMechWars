b43407d03d33fc604fd8c2e1d96a35293ba27365
yusifimanov <127264986+todoVV@users.noreply.github.com>
2024-01-26 14:00:52 -0800

    Project

---
+++ Source/VRMechWarsCPP/VRMechWarsCPPGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VRMechWarsCPPGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VRMECHWARSCPP_API AVRMechWarsCPPGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
